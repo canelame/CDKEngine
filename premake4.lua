3da9fde60c8d20f05c831aef9d057ff8ba3818e0
canelame <canelame@esat-alumni.com>
2015-10-09 23:29:50 +0200

    add extra files

---
+++ premake4.lua
@@ -0,0 +1,20 @@
+-- Create Solution
+
+solution "SolutionTestGenie"
+
+configurations{"Debug","Release"}
+location("debug")
+targetdir("debug/bin")
+debugdir("debug/bin")
+
+
+project "ProjectTestGenie"
+kind "ConsoleApp"
+language "C++"
+defines {"_GLFW_WIN32","_GLFW_WGL","_GLFW_USE_OPENGL","GLEW_STATIC"}
+links{"opengl32"}
+includedirs{"include"}
+files{"include/**.h","src/**.cc","src/**.c"}
+configuration "Debug"
+
+flags "Symbols"
